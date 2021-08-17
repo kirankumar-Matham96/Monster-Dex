214693a312233b208006d3fc92f93258319d66b0
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-08-17 12:46:37 +0530

    Monster-dex app for deploy in git

---
+++ monster_dex_app/src/components/card/card-component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/card.component.scss';
+
+const Card = (props) => {
+  return (
+    <div className='card-container'>
+      <img alt="monster-img" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
+      <h1>{props.monster.name}</h1>
+      <p>{props.monster.email}</p>
+    </div>
+  )
+}
+
+export default Card;
